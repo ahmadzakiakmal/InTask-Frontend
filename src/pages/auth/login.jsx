5f576956d179ea062806208c51f789b1f0a36e9f
salwamhrn11 <salwamaharani920@gmail.com>
2023-11-09 21:01:40 +0700

    fix: register and login page

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -7,10 +7,12 @@ export default function Login() {
   const router = useRouter();
   return (
     <main className="flex flex-col justify-center items-center min-h-screen" style = {{ backgroundColor: "#1B2430" }}>
-      <section className="p-10 px-12 bg-yellow rounded-[20px]  w-[350px] h-[490px] flex flex-col gap-4 justify-center items-center">
+      <section className="p-10 px-12 bg-yellow rounded-[20px]  w-[350px] h-[500px] flex flex-col gap-4 justify-center items-center">
         <div className="p-2 bg-navy rounded-[10px] grid place-items-center">
-          <Image src={InTaskLogo} alt="InTask Logo" className="w-[150px]" />
+          <Image src={InTaskLogo} alt="InTask Logo" className="w-[135px]" />
         </div>
+        <p className="text-[20px] font-bold mt-0" style = {{color:  "#1B2430", marginTop: "-0.4rem"}}>Login</p>
+        <p className="text-[12px] mt-0" style = {{color:  "#1B2430", marginTop: "-0.9rem"}}>Welcome back! Enter your details here!</p>
 
         <form className="flex flex-col gap-2"
           onSubmit={(e) => {
--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -5,10 +5,12 @@ import { useRouter } from "next/router";
 export default function Register() {
   return(
     <main className="flex flex-col justify-center items-center min-h-screen" style = {{ backgroundColor: "#1B2430" }}>
-    <section className="p-10 px-12 bg-yellow rounded-[20px]  w-[350px] h-[490px] flex flex-col gap-4 justify-center items-center">
+    <section className="p-10 px-12 bg-yellow rounded-[20px]  w-[350px] h-[500px] flex flex-col gap-4 justify-center items-center">
       <div className="p-2 bg-navy rounded-[10px] grid place-items-center">
-        <Image src={InTaskLogo} alt="InTask Logo" className="w-[150px]" />
+        <Image src={InTaskLogo} alt="InTask Logo" className="w-[135px]" />
       </div>
+      <p className="text-[20px] font-bold mt-0" style = {{color:  "#1B2430", marginTop: "-0.4rem"}}>Register</p>
+        <p className="text-[12px] mt-0" style = {{color:  "#1B2430", marginTop: "-0.9rem"}}>Great idea to create your account now!</p>
 
       <form className="flex flex-col gap-2"
         onSubmit={(e) => {
@@ -24,7 +26,7 @@ export default function Register() {
           Password
           <input className="outline" />
         </label>
-        <p className="text-[14px]">Have&apos; an account? <Link href="/auth/login" className="underline">Log in</Link>.</p>
+        <p className="text-[14px]"> Already have an account? <Link href="/auth/login" className="underline">Log in</Link>.</p>
         <button type="submit" className="outline py-1">Register</button>
       </form>
     </section>
