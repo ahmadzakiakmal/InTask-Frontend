51403fd84f7fdaf190f9e089aa10ce16781a7d3c
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-11-11 16:09:41 +0700

    feat: setup react-toastify

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,11 @@
 import "@/styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <main>
+      <ToastContainer position="bottom-right" />
       <Component {...pageProps} />
     </main>
   );
