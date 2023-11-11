055e6562a9232ee365238c844296bff301b5e5f5
salwamhrn11 <salwamaharani920@gmail.com>
2023-11-11 09:29:20 +0700

    fix: add full name

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 export default function Register() {
   return(
     <main className="flex flex-col justify-center items-center min-h-screen" style = {{ backgroundColor: "#1B2430" }}>
-    <section className="p-10 px-12 bg-yellow rounded-[20px]  w-[350px] h-[500px] flex flex-col gap-4 justify-center items-center">
+    <section className="p-10 px-12 bg-yellow rounded-[20px]  w-[350px] h-[550px] flex flex-col gap-4 justify-center items-center">
       <div className="p-2 bg-navy rounded-[10px] grid place-items-center">
         <Image src={InTaskLogo} alt="InTask Logo" className="w-[135px]" />
       </div>
@@ -18,6 +18,10 @@ export default function Register() {
           router.replace("/dashboard");
         }}
       >
+        <label className="flex flex-col gap-1">
+          Full Name
+          <input className="outline" />
+        </label>
         <label className="flex flex-col gap-1">
           Username / Email
           <input className="outline" />
