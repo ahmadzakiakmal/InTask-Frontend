1658f887ffcc6c05e39e22efc20eda773f248287
salwamhrn11 <salwamaharani920@gmail.com>
2023-11-12 20:53:10 +0700

    fix: edit status badges

--- src/components/StatusBadge.jsx
+++ src/components/StatusBadge.jsx
@@ -1,14 +1,14 @@
 export default function StatusBadge({ status }) {
   if (status === "ongoing")
     return (
-      <div className="w-fit px-2 outline outline-[0.2px] rounded-[5px] outline-[#A3E9FF] text-[#94FFFF]" style={{ backgroundColor: 'rgba(163, 233, 255, 0.15)'}}>
+      <div className="w-fit px-2 outline outline-[0.2px] rounded-[5px] outline-[#A3E9FF] bg-[rgba(163, 233, 255, 0.15)] text-[#94FFFF]">
         In Progress
       </div>
     );
 
   if (status === "done")
     return (
-      <div className="w-fit px-2 outline outline-[0.2px] rounded-[5px] outline-[#D2C5FF] text-[#D2C5FF]" style={{ backgroundColor: 'rgba(150,119,255,0.15'}}>
+      <div className="w-fit px-2 outline outline-[0.2px] rounded-[5px] outline-[#D2C5FF] bg-[rgba(150,119,255,0.15)] text-[#D2C5FF]">
         Done
       </div>
     );
