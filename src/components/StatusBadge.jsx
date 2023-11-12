647ed36c62892052f9dd1a0aed8c98b50ce1088e
salwamhrn11 <salwamaharani920@gmail.com>
2023-11-12 20:50:49 +0700

    fix: edit status badges

--- src/components/StatusBadge.jsx
+++ src/components/StatusBadge.jsx
@@ -1,22 +1,19 @@
 export default function StatusBadge({ status }) {
   if (status === "ongoing")
     return (
-      // TODO: Change colors
-      <div className="w-fit px-2 outline outline-[0.2px] rounded-[5px] outline-green bg-[rgba(94,238,126,0.15)] text-green">
-        On Going
+      <div className="w-fit px-2 outline outline-[0.2px] rounded-[5px] outline-[#A3E9FF] text-[#94FFFF]" style={{ backgroundColor: 'rgba(163, 233, 255, 0.15)'}}>
+        In Progress
       </div>
     );
 
   if (status === "done")
     return (
-      // TODO: Change colors
-      <div className="w-fit px-2 outline outline-[0.2px] rounded-[5px] outline-green bg-[rgba(94,238,126,0.15)] text-green">
+      <div className="w-fit px-2 outline outline-[0.2px] rounded-[5px] outline-[#D2C5FF] text-[#D2C5FF]" style={{ backgroundColor: 'rgba(150,119,255,0.15'}}>
         Done
       </div>
     );
 
   return (
-    // TODO: Change colors
     <div className="w-fit px-2 outline outline-[0.2px] rounded-[5px] outline-green bg-[rgba(94,238,126,0.15)] text-green">
       To Do
     </div>
