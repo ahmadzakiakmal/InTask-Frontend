cafedde48b9af841a4fe6fe9d40d1016980ed6bf
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-11-16 05:19:46 +0700

    feat: map projects in projects page

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,12 +1,68 @@
 import Layout from "@/components/dashboard/Layout";
+import ProjectCardComponent from "@/components/ProjectCard";
 import ProjectNavbar from "@/components/ProjectNavbar";
 
 export default function Dashboard() {
+  const projects = [
+    {
+      title: "Project 1",
+      id: 1,
+      color: "yellow"
+    },
+    {
+      title: "Project 2",
+      id: 2,
+      color: "purple"
+    },
+    {
+      title: "Project 3",
+      id: 3,
+      color: "orange"
+    },
+    {
+      title: "Project 4",
+      id: 4,
+      color: "peach"
+    },
+    {
+      title: "Project 5",
+      id: 5,
+      color: "blue"
+    },
+    {
+      title: "Project 6",
+      id: 6,
+      color: "green"
+    },
+    {
+      title: "Project 6",
+      id: 6,
+      color: "violet"
+    },
+    {
+      title: "Project 6",
+      id: 6,
+      color: "pink"
+    },
+    {
+      title: "Project 6",
+      id: 6,
+      color: "cream"
+    },
+  ];
   return(
     <Layout>
       <main className="flex  flex-col items-center h-full">
         <ProjectNavbar/>
-        <h1>Projects Page</h1>
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 w-full gap-8 mt-8" >
+          {
+            projects.map((project) => {
+              return(
+                <ProjectCardComponent key={project.id} title={project.title} color={project.color}/>
+              );
+            })
+          }
+        </section>
       </main>
     </Layout>
   );
