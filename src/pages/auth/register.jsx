4fe6ed8bec70fc7142f2e7aef01bb3a8f93bec06
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-11-16 05:40:17 +0700

    fix: refactor register page

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -14,14 +14,27 @@ export default function Register() {
   const [password, setPassowrd] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  return(
-    <main className="flex flex-col justify-center items-center min-h-screen" style = {{ backgroundColor: "#1B2430" }}>
-    <section className="p-10 px-12 bg-yellow rounded-[20px]  w-[350px] h-[565px] flex flex-col gap-4 justify-center items-center">
-      <div className="p-2 bg-navy rounded-[10px] grid place-items-center">
-        <Image src={InTaskLogo} alt="InTask Logo" className="w-[135px]" />
-      </div>
-      <p className="text-[20px] font-bold mt-0" style = {{color:  "#1B2430", marginTop: "-0.4rem"}}>Register</p>
-        <p className="text-[12px] mt-0" style = {{color:  "#1B2430", marginTop: "-0.9rem"}}>Great idea to create your account now!</p>
+  return (
+    <main
+      className="flex flex-col justify-center items-center min-h-screen"
+      style={{ backgroundColor: "#1B2430" }}
+    >
+      <section className="p-10 px-12 bg-yellow rounded-[20px]  w-[350px] flex flex-col gap-4 justify-center items-center my-10">
+        <div className="p-2 bg-navy rounded-[10px] grid place-items-center">
+          <Image src={InTaskLogo} alt="InTask Logo" className="w-[135px]" />
+        </div>
+        <p
+          className="text-[20px] font-bold mt-0"
+          style={{ color: "#1B2430", marginTop: "-0.4rem" }}
+        >
+          Register
+        </p>
+        <p
+          className="text-[12px] mt-0"
+          style={{ color: "#1B2430", marginTop: "-0.9rem" }}
+        >
+          Great idea to create your account now!
+        </p>
 
         <form
           className="flex flex-col gap-2"
@@ -47,7 +60,7 @@ export default function Register() {
               });
           }}
         >
-        <label className="flex flex-col gap-1" style={{marginTop:'-0.8rem'}}>
+          <label className="flex flex-col gap-1">
             Name
             <input
               className="outline"
@@ -55,23 +68,54 @@ export default function Register() {
               onChange={(e) => setName(e.target.value)}
             />
           </label>
-        <label className="flex flex-col gap-1">
-          Email
-          <input className="outline" type="email" value={email} onChange={(e)=> setEmail(e.target.value)} />
-        </label>
-        <label className="flex flex-col gap-1">
-          Password
-          <input className="outline" type="password" value={password} onChange={(e)=> setConfirmPassword(e.target.value)} />
-        </label>
-        <label type="password" className="flex flex-col gap-1">
-            Confirm Password
-            <input type="password" className="outline" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}
+          <label className="flex flex-col gap-1">
+            Email
+            <input
+              type="email"
+              className="outline"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-        </label>
-        <p className="text-[14px]"> {" "}Already have an account? {" "}<Link href="/auth/login" className="underline">Log in</Link>.</p>
-        <button type="submit" className="outline py-1">Register</button>
-      </form>
-    </section>
-  </main>
-);
+          </label>
+          <label className="flex flex-col gap-1">
+            Username
+            <input
+              className="outline"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label className="flex flex-col gap-1">
+            Password
+            <input
+              type="password"
+              className="outline"
+              value={password}
+              onChange={(e) => setPassowrd(e.target.value)}
+            />
+          </label>
+          <label type="password" className="flex flex-col gap-1">
+            Confirm Password
+            <input
+              type="password"
+              className="outline"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </label>
+          <p className="text-[14px]">
+            {" "}
+            Already have an account?{" "}
+            <Link href="/auth/login" className="underline">
+              Log in
+            </Link>
+            .
+          </p>
+          <button type="submit" className="outline py-1">
+            Register
+          </button>
+        </form>
+      </section>
+    </main>
+  );
 }
