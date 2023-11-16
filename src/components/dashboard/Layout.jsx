b95b4cb63f8d9b2ed9f84cb1967e852100551670
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-11-16 20:47:26 +0700

    fix: further improve responsiveness

--- src/components/dashboard/Layout.jsx
+++ src/components/dashboard/Layout.jsx
@@ -2,12 +2,12 @@ import MenuComponent from "../Menu";
 
 export default function Layout({ children }) {
   return (
-    <main className="flex flex-row-reverse sm:flex-row bg-neutral gap-5 sm:gap-8 lg:gap-10 px-[5%] xs:px-5 sm:px-8 lg:px-10 py-10">
+    <main className="flex flex-row-reverse sm:flex-row bg-neutral gap-5 sm:gap-8 lg:gap-10 px-[5%] xs:px-5 sm:px-8 lg:px-10">
       <aside className="flex-shrink-0 min-h-screen bg-transparent">
         <MenuComponent />
       </aside>
 
-      <section className="text-navy w-full">{children}</section>
+      <section className="text-navy w-full py-10">{children}</section>
     </main>
   );
 }
