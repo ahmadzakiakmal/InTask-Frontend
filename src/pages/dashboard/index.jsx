744ff600186bb4a2c5fa8cbb14966e23e2729562
Nc Galih <encegalih@gmail.com>
2023-11-27 15:42:29 +0700

    feat: api integration - get projects

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,8 +1,20 @@
 import Layout from "@/components/dashboard/Layout";
 import ProjectCardComponent from "@/components/ProjectCard";
 import ProjectNavbar from "@/components/ProjectNavbar";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 export default function Dashboard() {
+  const [project, setProject] = useState([])
+  const username = 'ncgalih'
+  useEffect(()=>{
+    axios.get(process.env.NEXT_PUBLIC_API_URL + '/project/' + username)
+    .then(res=> {
+      setProject(res.data.projects)
+    })
+    .catch(e=>{toast.info('err ' + e.message)})
+  }, [])
   const projects = [
     {
       title: "Project 1",
@@ -55,13 +67,11 @@ export default function Dashboard() {
       <main className="flex flex-col items-center h-full">
         <ProjectNavbar/>
         <section className="grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 w-full gap-6 lg:gap-8 mt-8" >
-          {
-            projects.map((project) => {
-              return(
-                <ProjectCardComponent key={project.id} title={project.title} color={project.color}/>
-              );
-            })
-          }
+          {project.map(project => {
+            return(
+              <ProjectCardComponent key={project._id} title={project.title} color={'purple'}/>
+            )
+          })}
         </section>
       </main>
     </Layout>
