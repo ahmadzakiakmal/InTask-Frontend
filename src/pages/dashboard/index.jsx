3064dd2b86a702794bb47de1a20b561f0f9cde7d
Nc Galih <encegalih@gmail.com>
2023-11-27 15:55:07 +0700

    feat: add colors variation of project card

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -8,6 +8,7 @@ import { toast } from "react-toastify";
 export default function Dashboard() {
   const [project, setProject] = useState([])
   const username = 'ncgalih'
+
   useEffect(()=>{
     axios.get(process.env.NEXT_PUBLIC_API_URL + '/project/' + username)
     .then(res=> {
@@ -15,61 +16,21 @@ export default function Dashboard() {
     })
     .catch(e=>{toast.info('err ' + e.message)})
   }, [])
-  const projects = [
-    {
-      title: "Project 1",
-      id: 1,
-      color: "yellow"
-    },
-    {
-      title: "Project 2",
-      id: 2,
-      color: "purple"
-    },
-    {
-      title: "Project 3",
-      id: 3,
-      color: "orange"
-    },
-    {
-      title: "Project 4",
-      id: 4,
-      color: "peach"
-    },
-    {
-      title: "Project 5",
-      id: 5,
-      color: "blue"
-    },
-    {
-      title: "Project 6",
-      id: 6,
-      color: "green"
-    },
-    {
-      title: "Project 6",
-      id: 7,
-      color: "violet"
-    },
-    {
-      title: "Project 6",
-      id: 8,
-      color: "pink"
-    },
-    {
-      title: "Project 6",
-      id: 9,
-      color: "cream"
-    },
-  ];
+
+  const colors = ['yellow', 'purple', 'orange']
+  const getColor = (id) => {
+    const idx = id % (colors.length)
+    return colors[idx];
+  }
+
   return(
     <Layout>
       <main className="flex flex-col items-center h-full">
         <ProjectNavbar/>
         <section className="grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 w-full gap-6 lg:gap-8 mt-8" >
-          {project.map(project => {
+          {project.map((project, id) => {
             return(
-              <ProjectCardComponent key={project._id} title={project.title} color={'purple'}/>
+              <ProjectCardComponent key={project._id} title={project.title} color={getColor(id)}/>
             )
           })}
         </section>
