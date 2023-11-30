b99f8a19819d0aeb0083a62c95c40fdca0bcd71e
Nc Galih <encegalih@gmail.com>
2023-11-30 23:21:32 +0700

    feat: API Integration - delete task

--- src/components/ToDoItemRow.jsx
+++ src/components/ToDoItemRow.jsx
@@ -3,9 +3,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import EditTaskStatusModal from "./modals/EditTaskStatus";
 import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export default function ToDoItemRow({ task, projectId, onSuccess }) {
   const [editStatusOpen, setStatusOpen] = useState(false);
+
+  const handleDelete = () => {
+    axios.delete(process.env.NEXT_PUBLIC_API_URL + '/project/' + projectId + '/tasks/' + task._id, 
+    { withCredentials: true })
+    .then(()=>{
+      onSuccess()
+      toast.info(`deleted ${task.name}`)
+    })
+    .catch(e => {
+      toast.error('failed to delete ' + e.message)
+    })
+  }
+
   return (
     <tr className="mb-2">
       <td className="text-center w-[60px] py-2 border-r border-yellow">1</td>
@@ -25,7 +40,7 @@ export default function ToDoItemRow({ task, projectId, onSuccess }) {
           taskId={task?._id} />
       </td>
       <td className="mb-2 bg-red-900 border-l border-b border-yellow">
-        <button className="w-full">
+        <button className="w-full" onClick={handleDelete}>
           <FontAwesomeIcon icon={faTrashCan} style={{ color: "#ffffff" }} />
         </button>
       </td>
