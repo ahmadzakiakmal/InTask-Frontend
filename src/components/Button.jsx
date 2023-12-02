5757ba60e9ae2c58314d54e5344c6b50b93e01f4
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-02 21:07:16 +0700

    feat: add button hover effects

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({ text, onClick }) {
   return (
     <button 
       onClick={onClick}
-      className="w-max rounded-[10px] py-2 px-3 bg-purple-200 font-semibold text-white">
+      className="w-max rounded-[10px] py-2 px-3 bg-purple-200 hover:bg-[#9676B0] transition duration-200 font-semibold text-white">
       <span>{text}</span>
     </button>
   );
