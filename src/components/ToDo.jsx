2510cabe9300751611cd2def09918bccbdd29d45
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-02 18:02:58 +0700

    feat: use tailwind instead of sTyLeS
    add shadow

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,21 +1,21 @@
 import { useDraggable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
 
-export default function ToDoItem({taskname, id}) {
-  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+export default function ToDoItem({ taskname, id }) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: id
-  })
+  });
   const style = {
     transform: CSS.Translate.toString(transform),
   };
   return (
-      <button 
-        className="w-full py-2 text-left bg-navy top-0 px-5 rounded-[10px]"
-        style={{ background:"#4B4B4B", ...style }}
-        ref={setNodeRef} 
-        {...listeners} 
-        {...attributes}>
-        <span style = {{ fontSize:"16px", fontWeight:"regular", color: "#D6D5A8" }}>{taskname}</span>
-      </button>
+    <button 
+      className="w-full py-2 text-left top-0 px-5 rounded-[10px] bg-[#4B4B4B] shadow-navy shadow-lg"
+      style={{ ...style }}
+      ref={setNodeRef} 
+      {...listeners} 
+      {...attributes}>
+      <span style = {{ fontSize:"16px", fontWeight:"regular", color: "#D6D5A8" }}>{taskname}</span>
+    </button>
   );
 }
