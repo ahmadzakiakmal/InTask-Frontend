89cb4f22e8c1c93619e578bb8793ec97dc0f655e
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-03 08:31:31 +0700

    fix: fix image tag + effects

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -119,83 +119,83 @@ export default function Home() {
 
       {/* //? section fitur */}
       <section className="pt-10 mb-10 lg:pt-0 lg:mb-10 md:px-10 xl:px-auto flex flex-col-reverse w-full lg:flex-row justify-center lg:justify-evenly max-w-[1600px] items-center gap-10 xl:gap-0 min-h-screen">
-      <div className="container mx-auto py-20">
-      <p className="text-3xl lg:text-5xl font-semibold text-navy mt-3">Our Fitur</p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-16 py-10 lg:py-20">
-        <div className="flex flex-col gap-5 ">
-          <img src={DashboardPict} alt="Project Picture" />
-          <p className="text-2xl font-semibold">Project</p>
-          <div className="flex items-center gap-2">
-            <p className="text-base text-justify sm:text-lg text-gray-500 text-center lg:text-[15px] mb-10">
+        <div className="container mx-auto py-20">
+          <p className="text-3xl lg:text-5xl font-semibold text-navy mt-3">Our Fitur</p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-16 py-10 lg:py-20">
+            <div className="flex flex-col gap-5 ">
+              <Image src={DashboardPict} alt="Project Picture" className="shadow-[0_0_8px_rgba(0,0,0,.5)] rounded-[10px]" />
+              <p className="text-2xl font-semibold">Project</p>
+              <div className="flex items-center gap-2">
+                <p className="text-base text-justify sm:text-lg text-gray-500 text-center lg:text-[15px] mb-10">
               Pada fitur project, pengguna dapat membuat dan mengelola 
               project-project yang akan atau sedang dilakukan.
-            </p>
-          </div>
-        </div>
+                </p>
+              </div>
+            </div>
 
-        <div className="flex flex-col gap-5 ">
-          <img src={ProjectPict} alt="Isi Project" />
-          <p className="text-2xl font-semibold">Isi dari project</p>
-          <div className="flex items-center gap-2">            
-            <p className="text-base text-justify sm:text-lg text-gray-500 text-center lg:text-[15px] mb-10">
+            <div className="flex flex-col gap-5 ">
+              <img src={ProjectPict} alt="Isi Project" />
+              <p className="text-2xl font-semibold">Isi dari project</p>
+              <div className="flex items-center gap-2">            
+                <p className="text-base text-justify sm:text-lg text-gray-500 text-center lg:text-[15px] mb-10">
             Pengguna dapat mengisi tugas-tugas atau task yang akan dilakukan (to do), 
             yang sedang dikerjakan (doing), dan yang sudah selesai dikerjakan (done).
-            </p>
-          </div>
-        </div>
+                </p>
+              </div>
+            </div>
         
-        <div className="flex flex-col gap-5 ">
-          <img src={TaskPict} alt="Fitur Task" />
-          <p className="text-2xl font-semibold">Task</p>
-          <div className="flex items-center gap-2">
-            <p className="text-base text-justify sm:text-lg text-gray-500 text-center lg:text-[15px] mb-10">
+            <div className="flex flex-col gap-5 ">
+              <img src={TaskPict} alt="Fitur Task" />
+              <p className="text-2xl font-semibold">Task</p>
+              <div className="flex items-center gap-2">
+                <p className="text-base text-justify sm:text-lg text-gray-500 text-center lg:text-[15px] mb-10">
             Pada fitur Task, pengguna dapat membuat dan mengelola tugas-tugas individual dengan memberikan status tugas, seperti Todo, In Progress, dan Done. 
-            </p>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      </div>
       </section>
 
       {/* //? section anggota */}
       <section className="pt-10 mb-10 lg:pt-0 lg:mb-10 md:px-10 xl:px-auto flex flex-col-reverse w-full lg:flex-row justify-center lg:justify-evenly max-w-[1600px] items-center gap-10 xl:gap-0 min-h-screen">
-          <div className="container mx-auto py-20">
-            <p className="text-base lg:text-xl font-medium text-gray-500 uppercase">Kelompok 1</p>
-            <p className="text-3xl lg:text-5xl font-semibold text-navy mt-3">Meet our member!</p>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-16 py-10 lg:py-20">
-              <div className="flex flex-col gap-5 ">
-                <img src={Jaki} alt="Ahmad Zaki Akmal"/>
-                <p className="text-2xl font-semibold">Ahmad Zaki Akmal</p>
-                <div className="flex items-center gap-2">
-                  <p className="text-gray-500">21/480179/TK/52981</p>
-                </div>
+        <div className="container mx-auto py-20">
+          <p className="text-base lg:text-xl font-medium text-gray-500 uppercase">Kelompok 1</p>
+          <p className="text-3xl lg:text-5xl font-semibold text-navy mt-3">Meet our member!</p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-16 py-10 lg:py-20">
+            <div className="flex flex-col gap-5 ">
+              <img src={Jaki} alt="Ahmad Zaki Akmal"/>
+              <p className="text-2xl font-semibold">Ahmad Zaki Akmal</p>
+              <div className="flex items-center gap-2">
+                <p className="text-gray-500">21/480179/TK/52981</p>
               </div>
+            </div>
               
-              <div className="flex flex-col gap-5 ">
-                <img src={Salwa} alt="Salwa Maharani" />
-                <p className="text-2xl font-semibold">Salwa Maharani</p>
-                <div className="flex items-center gap-2">
-                  <p className="text-gray-500">21/481194/TK/53113</p>
-                </div>
+            <div className="flex flex-col gap-5 ">
+              <img src={Salwa} alt="Salwa Maharani" />
+              <p className="text-2xl font-semibold">Salwa Maharani</p>
+              <div className="flex items-center gap-2">
+                <p className="text-gray-500">21/481194/TK/53113</p>
               </div>
+            </div>
 
-              <div className="flex flex-col gap-5 ">
-                <img src={Duta} alt="Duta Pradana"/>
-                <p className="text-2xl font-semibold">Diestra Pradana</p>
-                <div className="flex items-center gap-2">
-                  <p className="text-gray-500">21/478179/TK/52693</p>
-                </div>
+            <div className="flex flex-col gap-5 ">
+              <img src={Duta} alt="Duta Pradana"/>
+              <p className="text-2xl font-semibold">Diestra Pradana</p>
+              <div className="flex items-center gap-2">
+                <p className="text-gray-500">21/478179/TK/52693</p>
               </div>
+            </div>
 
-              <div className="flex flex-col gap-5 ">
-                <img src={Galih} alt="Nicholas Galis"/>
-                <p className="text-2xl font-semibold">Nicholas Galih</p>
-                <div className="flex items-center gap-2">
-                  <p className="text-gray-500">21/482747/TK/53361</p>
-                </div>
+            <div className="flex flex-col gap-5 ">
+              <img src={Galih} alt="Nicholas Galis"/>
+              <p className="text-2xl font-semibold">Nicholas Galih</p>
+              <div className="flex items-center gap-2">
+                <p className="text-gray-500">21/482747/TK/53361</p>
               </div>
             </div>
           </div>
+        </div>
       </section>
 
     </main>
