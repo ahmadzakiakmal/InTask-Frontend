0cfd71e2d20a6dcb1a0c34c154de00c278829372
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-04 05:33:42 +0700

    fix: reading property toLowerCase of undefined
    Co-Authored-By: salwamhrn11 <78772324+salwamhrn11@users.noreply.github.com>

--- src/components/TextInput1.jsx
+++ src/components/TextInput1.jsx
@@ -12,7 +12,7 @@ export default function TextInput1({ label, value, onChange, ...prop }) {
         <input 
           className="flex-grow bg-neutral pl-2 py-1 w-[300px] rounded-md border border-purple-200 focus:outline-purple-200 text-black font-medium sm:text-sm"
           name={label}
-          placeholder={`Enter your ${label.toLowerCase()} here`}
+          placeholder={`Enter your ${label} here`}
           value={value}
           onChange={(e) => {
             console.log(`Input changed for ${label}:`, e.target.value);
