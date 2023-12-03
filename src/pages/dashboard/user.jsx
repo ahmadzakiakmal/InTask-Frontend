3e4e279f92ccc952166b29087514374d32128633
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-03 16:53:55 +0700

    feat: use loading context

--- src/pages/dashboard/user.jsx
+++ src/pages/dashboard/user.jsx
@@ -1,17 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "@/components/dashboard/Layout";
 import TextInput1 from "@/components/TextInput1";
 import Button from "@/components/Button";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { LoadingContext } from "@/context/LoadingContext";
 
 export default function UserPage() {
   const initialProfile = {
-    name: "Salwa Maharani",
-    email: "salwa@example.com",
-    phoneNumber: "123-456-7890",
+    name: "",
+    email: "",
   };
 
   const [profile, setProfile] = useState({ ...initialProfile });
   const [isEditing, setIsEditing] = useState(false);
+  const [refetch, setRefetch] = useState(false);
+  const [emoji, setEmoji] = useState("🙂");
+  const { setLoading } = React.useContext(LoadingContext);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.post(process.env.NEXT_PUBLIC_API_URL + "/user/search", {
+      searchQuery: localStorage.getItem("username"),
+    }, { withCredentials: true })
+      .then((res) => {
+        setProfile({
+          name: res.data.username,
+          email: res.data.email,
+        });
+        setEmoji(res.data.emoticon);
+      })
+      .catch(() => {
+        toast.error("Error fetching profile data");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [refetch]);
 
   const handleInputChange = (e) => {
     console.log("Input changed:", e.target.name, e.target.value);
@@ -27,66 +52,87 @@ export default function UserPage() {
   };
 
   const handleEditProfile = () => {
+    if (
+      profile.name === "" ||
+      profile.email === "" ||
+      profile.phoneNumber === ""
+    )
+      return toast.error("Cannot edit profile");
     setIsEditing(true);
   };
 
   return (
     <Layout>
-      <main className="flex flex-col items-center justify-center h-full" style={{ marginTop: "-2rem" }}>
-        <h1 className="font-semibold text-[42px] sm:text-xl lg:text-2xl">Your Profile</h1>
+      <main
+        className="flex flex-col items-center justify-center h-full"
+        style={{ marginTop: "-2rem" }}
+      >
+        <h1 className="font-semibold text-[42px] sm:text-xl lg:text-2xl">
+          Your Profile
+        </h1>
         <div className="flex flex-col items-center">
-          <span className="text-[100px] mb-4">🙂</span>
+          <span className="text-[100px] mb-4">{emoji}</span>
 
           {isEditing ? (
             <>
-              <TextInput1 label="Name" value={profile.name} name="name" onChange={handleInputChange} />
-              <TextInput1 label="Email" value={profile.email} name="email" onChange={handleInputChange} />
-              <TextInput1 label="Phone Number" value={profile.phoneNumber} name="phoneNumber" onChange={handleInputChange} />
+              <TextInput1
+                label="Name"
+                value={profile.name}
+                name="name"
+                onChange={handleInputChange}
+              />
+              <TextInput1
+                label="Email"
+                value={profile.email}
+                name="email"
+                onChange={handleInputChange}
+              />
             </>
           ) : (
             <>
-
               {/* buat bagian setelah save */}
-              <div className="mb-4 relative flex flex-col">
-                <div className="flex items-center" style={{ marginBottom:"1.5rem" }}>
-                  <div className="w-40 flex-shrink-0 pr-2 flex justify-between items-center"> 
-                    <span className="text-black font-semibold sm:text-sm whitespace-nowrap">Name </span>
+              <div className="relative flex flex-col">
+                <div
+                  className="flex items-center"
+                  style={{ marginBottom: "1.5rem" }}
+                >
+                  <div className="w-40 flex-shrink-0 pr-2 flex justify-between items-center">
+                    <span className="text-black font-semibold sm:text-sm whitespace-nowrap">
+                      Name{" "}
+                    </span>
                     <span className="ml-1">:</span>
                   </div>
-                  <div className="flex-grow bg-slate-500/20 cursor-not-allowed pl-2 py-1 w-[300px] rounded-md border border-purple-200 focus:outline-purple-200 text-black font-medium sm:text-sm">
+                  <div className="flex-grow h-[30px] bg-slate-500/20 cursor-not-allowed pl-2 py-1 w-[300px] rounded-md border border-purple-200 focus:outline-purple-200 text-black font-medium sm:text-sm">
                     {profile.name}
                   </div>
                 </div>
 
-                <div className="flex items-center" style={{ marginBottom:"1.5rem" }}>
-                  <div className="w-40 flex-shrink-0 pr-2 flex justify-between items-center"> 
-                    <span className="text-black font-semibold sm:text-sm whitespace-nowrap">Email </span>
+                <div
+                  className="flex items-center"
+                  style={{ marginBottom: "1.5rem" }}
+                >
+                  <div className="w-40 flex-shrink-0 pr-2 flex justify-between items-center">
+                    <span className="text-black font-semibold sm:text-sm whitespace-nowrap">
+                      Email{" "}
+                    </span>
                     <span className="ml-1">:</span>
                   </div>
-                  <div className="flex-grow bg-slate-500/20 cursor-not-allowed pl-2 py-1 w-[300px] rounded-md border border-purple-200 focus:outline-purple-200 text-black font-medium sm:text-sm">
+                  <div className="flex-grow h-[30px] bg-slate-500/20 cursor-not-allowed pl-2 py-1 w-[300px] rounded-md border border-purple-200 focus:outline-purple-200 text-black font-medium sm:text-sm">
                     {profile.email}
-                  </div>
-                </div>
-
-                <div className="flex items-center mb-2">
-                  <div className="w-40 flex-shrink-0 pr-2 flex justify-between items-center"> 
-                    <span className="text-black font-semibold sm:text-sm whitespace-nowrap">Phone Number </span>
-                    <span className="ml-1">:</span>
-                  </div>
-                  <div className="flex-grow bg-slate-500/20 cursor-not-allowed pl-2 py-1 w-[300px] rounded-md border border-purple-200 focus:outline-purple-200 text-black font-medium sm:text-sm">
-                    {profile.phoneNumber}
                   </div>
                 </div>
               </div>
             </>
-          )}          
+          )}
 
           <Button
-            className={"btn btn-sm lg:btn-lg bg-[#816797] rounded-[10px] font-medium text-white border-[#5F4C6F] w-44 lg:w-28 capitalize py-2"}
-            style={{ marginTop: "1.5rem" }}
+            className={
+              "bg-[#816797] rounded-[10px] font-medium text-white border-[#5F4C6F] capitalize "
+            }
             onClick={isEditing ? handleSaveProfile : handleEditProfile}
-            text={isEditing ? "Save Profile" : "Edit Profile"}>
-          </Button>
+            text={isEditing ? "Save Profile" : "Edit Profile"}
+            disabled={profile.name === "" || profile.email === ""}
+          ></Button>
         </div>
       </main>
     </Layout>
