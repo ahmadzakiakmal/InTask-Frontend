1a3ab39abfbb332d066683bf3f2e1debd5d4bab7
salwamhrn11 <salwamaharani920@gmail.com>
2023-12-03 15:29:33 +0700

    feat: alhamdulillah user page

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,15 +1,17 @@
 export default function TextInput({ label, ...prop }){
   return (
-    <div class="relative">
-      <div class="absolute inset-y-0 -left-2 pl-5 w-32 flex justify-between items-center">
+    <div class="relative flex flex-col mb-4">
+      <div className="flex items-center mb-2">
+      <div class="pl-5 w-32 flex justify-between items-center">
         <span class="text-black font-semibold sm:text-sm">{label}</span>
         <span>:</span>
       </div>
       <input 
         class="bg-neutral py-1 pl-32 rounded-md border border-purple-200 focus:outline-purple-200" 
         name={label}
-        placeholder="text input"
+        placeholder="Enter your answer here"
         {...prop}
       />
+      </div>
     </div>);
 }
