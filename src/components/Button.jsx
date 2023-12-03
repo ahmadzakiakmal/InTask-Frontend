865270692a94770838497d863d8e271356560e3a
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-03 16:25:44 +0700

    fix: add classname and disabled argument to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
-export default function Button({ text, onClick }) {
+export default function Button({ text, onClick, className, disabled }) {
   return (
     <button 
       onClick={onClick}
-      className="w-max rounded-[10px] py-2 px-3 bg-purple-200 hover:bg-[#9676B0] transition duration-200 font-semibold text-white">
+      disabled={disabled}
+      className={"w-max rounded-[10px] py-2 px-3 bg-purple-200 hover:bg-[#9676B0] transition duration-200 font-semibold text-white disabled:bg-purple-200/80 disabled:cursor-not-allowed " + className}>
       <span>{text}</span>
     </button>
   );
