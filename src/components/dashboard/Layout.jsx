7510c49705dbacb3ade7a63b08054bd1ea76b0d3
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-05 10:51:51 +0700

    fix: z-index issue and responsiveness

--- src/components/dashboard/Layout.jsx
+++ src/components/dashboard/Layout.jsx
@@ -3,11 +3,11 @@ import MenuComponent from "../Menu";
 export default function Layout({ children }) {
   return (
     <main className="flex flex-col-reverse xs:flex-row bg-neutral gap-5 sm:gap-8 lg:gap-10 px-[5%] xs:px-5 sm:px-8 lg:px-10">
-      <aside className="absolute sm:relative flex-shrink-0 h-fit sm:h-screen bg-transparent py-10 z-0">
+      <aside className="absolute sm:relative flex-shrink-0 h-fit sm:h-screen bg-transparent py-10">
         <MenuComponent />
       </aside>
 
-      <section className="text-navy w-full py-10 min-h-screen relative z-[0]">{children}</section>
+      <section className="text-navy w-full py-10 min-h-screen relative z-0">{children}</section>
     </main>
   );
 }
