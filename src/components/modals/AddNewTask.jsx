8230b98e46a551004ed9899ec9d1444e7deab25e
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-05 14:48:28 +0700

    feat: beautify assignee selection

--- src/components/modals/AddNewTask.jsx
+++ src/components/modals/AddNewTask.jsx
@@ -3,6 +3,8 @@ import Button from "../Button";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckSquare, faSquare } from "@fortawesome/free-solid-svg-icons";
 
 export default function NewTaskModal({
   isOpen,
@@ -100,9 +102,9 @@ export default function NewTaskModal({
               }}
             >
               {status && (
-                <p
+                <div
                   className={
-                    "px-2 py-1 " +
+                    "px-2 py-1 flex justify-start items-center " +
                     (status.toLowerCase() === "todo"
                       ? " outline-green outline outline-[0.2px] bg-[rgba(94,238,126,0.15)] text-green"
                       : "") +
@@ -117,7 +119,10 @@ export default function NewTaskModal({
                   {status.toLowerCase() === "todo" && "To Do"}
                   {status.toLowerCase() === "ongoing" && "Doing"}
                   {status.toLowerCase() === "done" && "Done"}
-                </p>
+                  <svg className="absolute right-[10px] " width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M25.3701 12.2449L15.9951 21.6199C15.8645 21.751 15.7092 21.8551 15.5383 21.926C15.3674 21.997 15.1841 22.0336 14.999 22.0336C14.8139 22.0336 14.6307 21.997 14.4597 21.926C14.2888 21.8551 14.1336 21.751 14.0029 21.6199L4.62793 12.2449C4.36375 11.9808 4.21533 11.6225 4.21533 11.2489C4.21533 10.8752 4.36375 10.5169 4.62793 10.2528C4.89211 9.98858 5.25041 9.84016 5.62402 9.84016C5.99763 9.84016 6.35593 9.98858 6.62012 10.2528L15.0002 18.6328L23.3803 10.2516C23.6445 9.9874 24.0028 9.83899 24.3764 9.83899C24.75 9.83899 25.1083 9.9874 25.3725 10.2516C25.6366 10.5158 25.7851 10.8741 25.7851 11.2477C25.7851 11.6213 25.6366 11.9796 25.3725 12.2438L25.3701 12.2449Z" fill="white"/>
+                  </svg>
+                </div>
               )}
               <div
                 className={
@@ -143,6 +148,7 @@ export default function NewTaskModal({
                 >
                   Done
                 </p>
+
               </div>
             </div>
           </label>
@@ -170,8 +176,8 @@ export default function NewTaskModal({
   );
 }
 
-function AssigneeOption({ assignee, setSelectedAssignees }) {
-  const [selected, setSelected] = useState(false);
+function AssigneeOption({ assignee, setSelectedAssignees, index }) {
+  const [selected, setSelected] = useState(index === 0 ? true : false);
   return (
     <div
       className={
@@ -192,7 +198,10 @@ function AssigneeOption({ assignee, setSelectedAssignees }) {
         });
       }}
     >
-      <span>{assignee}</span>
+      <span>{assignee}
+        {!selected ? <FontAwesomeIcon icon={faSquare} className="ml-2" /> :
+          <FontAwesomeIcon icon={faCheckSquare} className="ml-2" />}
+      </span>
     </div>
   );
 }
