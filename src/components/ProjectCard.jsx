675fb87ac27a938141447e43721b647572914dea
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-05 07:12:31 +0700

    fix: tidy up task display

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,7 +7,7 @@ export default function ProjectCardComponent({
   description,
   id,
   owner,
-  tasks
+  tasks,
 }) {
   const colors = {
     yellow: "bg-yellow",
@@ -22,9 +22,7 @@ export default function ProjectCardComponent({
   };
   return (
     <Link href={`/dashboard/project/${id}`}>
-      <div
-        className="relative w-full rounded-[15px] bg-navy cursor-pointer transition duration-200 hover:shadow-[0_0_8px_rgb(27,36,48,.9)]"
-      >
+      <div className="relative w-full rounded-[15px] bg-navy cursor-pointer transition duration-200 hover:shadow-[0_0_8px_rgb(27,36,48,.9)]">
         <div
           className={
             "w-full py-3 px-8 text-center rounded-t-[15px] mt-[-2px] " +
@@ -34,14 +32,32 @@ export default function ProjectCardComponent({
           <span className="font-semibold">{title}</span>
         </div>
         <div className="w-full h-fit px-3 py-4 text-white">
-          <p>Owner: {owner}</p>
-          <p>{description}</p>
-          <p className="text-green">
-            todo task : {tasks?.filter(t => t.status === "todo")?.length}
+          <p>
+            Owner:{" "}
+            <span className="bg-purple-100 rounded-[5px] px-2 py-1 text-white">
+              {owner}
+            </span>
           </p>
-          <p className="text-[#94FFFF]">
-            doing task : {tasks?.filter(t => t.status === "ongoing")?.length}
-          </p>
+          <p className="mt-1">{description}</p>
+          <hr className="my-1" />
+          <div className="text-green flex">
+            <div className="min-w-[100px] flex justify-between">
+              To Do <span className="mr-2">:</span>
+            </div>
+            {tasks?.filter((t) => t.status === "todo")?.length}
+          </div>
+          <div className="text-[#94FFFF] flex">
+            <div className="min-w-[100px] flex justify-between">
+              On Going <span className="mr-2">:</span>
+            </div>
+            {tasks?.filter((t) => t.status === "ongoing")?.length}
+          </div>
+          <div className="text-[#D2C5FF] flex">
+            <div className="min-w-[100px] flex justify-between">
+              Done <span className="mr-2">:</span>
+            </div>
+            {tasks?.filter((t) => t.status === "done")?.length}
+          </div>
         </div>
       </div>
     </Link>
