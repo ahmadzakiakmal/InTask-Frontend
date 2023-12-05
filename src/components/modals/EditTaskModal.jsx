80f717779404b7b85b6ab8217d64fd354b3184f5
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-05 15:29:40 +0700

    feat: update assignee selection
    - seadanya dulu disuruh pergi :v

--- src/components/modals/EditTaskModal.jsx
+++ src/components/modals/EditTaskModal.jsx
@@ -3,6 +3,9 @@ import Button from "../Button";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckSquare, faSquare } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/router";
 
 export default function EditTaskModal({
   isOpen,
@@ -13,66 +16,27 @@ export default function EditTaskModal({
 }) {
   Modal.setAppElement("#__next");
   const [searchQuery, setSearchQuery] = useState("");
-  const [assignees, setAssignees] = useState(task.assignees);
+  const [assignees, setAssignees] = useState();
+  const [selectedAssignees, setSelectedAssignees] = useState(task.assignees);
   const [name, setName] = useState(task.name);
   const [description, setDescription] = useState(task.description);
   const [status, setStatus] = useState(task.status);
   const [openDropdown, setOpenDropdown] = useState(false);
-  
+  const router = useRouter();
+
   useEffect(() => {
     setStatus(task.status);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isOpen]);
 
-  const searchUser = () => {
-    // prevent duplicate
-    if (
-      assignees.some((assignee) => {
-        return assignee.username === searchQuery;
-      })
-    )
-      return toast.error("User already added!");
-    axios
-      .post(
-        process.env.NEXT_PUBLIC_API_URL + "/user/search",
-        {
-          searchQuery: searchQuery,
-        },
-        {
-          withCredentials: true,
-        }
-      )
-      .then((res) => {
-        if (res.data.length === 0) {
-          toast.error("No user found!");
-          return;
-        }
-        if (assignees.includes(res.data.username)) {
-          toast.error("User already added!");
-          return;
-        }
-        const newAssignees = [
-          ...assignees,
-          {
-            emoticon: res.data.emoticon,
-            username: res.data.username,
-          },
-        ];
-        setAssignees(newAssignees);
-      })
-      .catch(() => {
-        toast.error("User not found!");
-        // console.log(err);
-      })
-      .finally(() => {
-        setSearchQuery("");
-      });
-  };
-
   const putUpdatedTask = () => {
     axios
       .put(
-        process.env.NEXT_PUBLIC_API_URL + "/project/" + projectId + "/tasks/" + task._id,
+        process.env.NEXT_PUBLIC_API_URL +
+          "/project/" +
+          projectId +
+          "/tasks/" +
+          task._id,
         {
           name,
           description,
@@ -190,63 +154,51 @@ export default function EditTaskModal({
             <div>
               Assignees
               <div className="flex gap-3 flex-wrap">
-                {assignees.map((assignee, index) => {
+                {assignees?.map((assignee, index) => {
                   return (
-                    <div
-                      className={
-                        "rounded-[5px] py-1 bg-purple-100 px-2 text-white flex gap-2"
-                      }
-                      key={assignee.username}
-                    >
-                      <span>
-                        {assignee.emoticon} {assignee.username}
-                      </span>
-                      <div
-                        className="hover:text-red-200 px-1 block w-min cursor-pointer select-none"
-                        onClick={() => {
-                          const newAssignees = [...assignees];
-                          newAssignees.splice(index, 1);
-                          setAssignees(newAssignees);
-                        }}
-                      >
-                        &times;
-                      </div>
-                    </div>
+                    <AssigneeOption key={assignee} assignee={assignee} selectedAssignees={selectedAssignees} setSelectedAssignees={setSelectedAssignees} index={index} />
                   );
                 })}
               </div>
-            </div>
-            <div className="flex gap-3 bg-white w-full pl-1 pr-2 py-1 rounded-[4px]">
-              <input
-                className="focus:outline px-2 py-1 rounded-[4px] w-full"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-              />
-              <button
-                className="flex-shrink-0"
-                onClick={(e) => {
-                  e.preventDefault();
-                  searchUser();
-                }}
-              >
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M19.6 21L13.3 14.7C12.8 15.1 12.225 15.4167 11.575 15.65C10.925 15.8833 10.2333 16 9.5 16C7.68333 16 6.146 15.3707 4.888 14.112C3.63 12.8533 3.00067 11.316 3 9.5C3 7.68333 3.62933 6.146 4.888 4.888C6.14667 3.63 7.684 3.00067 9.5 3C11.3167 3 12.8543 3.62933 14.113 4.888C15.3717 6.14667 16.0007 7.684 16 9.5C16 10.2333 15.8833 10.925 15.65 11.575C15.4167 12.225 15.1 12.8 14.7 13.3L21 19.6L19.6 21ZM9.5 14C10.75 14 11.8127 13.5627 12.688 12.688C13.5633 11.8133 14.0007 10.7507 14 9.5C14 8.25 13.5627 7.18767 12.688 6.313C11.8133 5.43833 10.7507 5.00067 9.5 5C8.25 5 7.18767 5.43767 6.313 6.313C5.43833 7.18833 5.00067 8.25067 5 9.5C5 10.75 5.43767 11.8127 6.313 12.688C7.18833 13.5633 8.25067 14.0007 9.5 14Z"
-                    fill="#D6D5A8"
-                  />
-                </svg>
-              </button>
             </div>
           </label>
           <Button text="Edit" type="submit" />
         </form>
       </div>
     </Modal>
+  );
+}
+
+function AssigneeOption({ assignee, selectedAssignees, setSelectedAssignees, index }) {
+  const [selected, setSelected] = useState(selectedAssignees.includes(assignee));
+  return (
+    <div
+      className={
+        "rounded-[5px] py-1 bg-purple-100 px-2 text-white flex gap-2 cursor-pointer " +
+        (selected
+          ? " outline outline-2 outline-offset-2 outline-purple-100"
+          : "")
+      }
+      key={assignee}
+      onClick={() => {
+        setSelected(!selected);
+        setSelectedAssignees((prev) => {
+          if (selected) {
+            return prev.filter((p) => p !== assignee);
+          } else {
+            return [...prev, assignee];
+          }
+        });
+      }}
+    >
+      <span>
+        {assignee}
+        {!selected ? (
+          <FontAwesomeIcon icon={faSquare} className="ml-2" />
+        ) : (
+          <FontAwesomeIcon icon={faCheckSquare} className="ml-2" />
+        )}
+      </span>
+    </div>
   );
 }
